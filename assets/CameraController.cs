1186dd638713d7614839348850d1bc2ad520cf04
vienthl <truonghuynhlongvien@gmail.com>
2026-06-04 04:10:03 +0700

    done map level 1

---
+++ assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); // A/D hoặc mũi tên
+        float v = Input.GetAxis("Vertical");   // W/S hoặc mũi tên
+
+        transform.Translate(h * moveSpeed * Time.deltaTime,
+                            v * moveSpeed * Time.deltaTime, 0);
+    }
+}
